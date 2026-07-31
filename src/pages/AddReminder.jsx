8d1876b80640ec5963c5961d50882a761e751a36
divya <pulamidivya26@gmail.com>
2026-07-31 13:20:10 -0400

    commit add-reminder page done!

--- src/pages/AddReminder.jsx
+++ src/pages/AddReminder.jsx
@@ -2,53 +2,157 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 import Navbar from "../components/Navbar";
+// import './index.css'
 
-const API_URL = "http://localhost:3000"
+const API_URL = "http://localhost:3000";
 
 export default function AddReminder() {
+  const Navigate = useNavigate();
   const [form, setForm] = useState({
     petId: "",
     task: "",
-    dueDate: null,
+    dueDate: "",
     notes: "",
+    // isDone: "",
   });
-  const [pets, setPets] = useState([])
+  const [pets, setPets] = useState([]);
+  const [selectedPetId, setSelectedPetId] = useState([]);
+  const [formError, setFormError] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchPet() {
-            try {
-                const response = await fetch("API_URL/pets")
-                console.log(response)
-                if (!response.ok) {
-                    throw new Error(`Server responded with ${response.status}`);
-                }
-                const data = await response.json();
-                setPets(data)
-                console.log(data)
-            } catch (err) {
-                console.error(err.message)
-            } 
+      try {
+        const response = await fetch(`${API_URL}/pets`);
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`Server responded with ${response.status}`);
         }
-    fetchPet()
+        const data = await response.json();
+        setPets(data);
+        setForm({ ...form, petId: data[0].petId });
+        console.log(data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+    fetchPet();
+  }, []);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-  },[])
-  // get all the pets in a useEffect
-  // set them all to a state
-  // use the select component 
+    if (!form.task) {
+      setFormError("Task is required!!!");
+      return;
+    }
+    setFormError("");
 
-  //function to create the reminder form
+    try {
+      console.log(form);
+      const response = await fetch(`${API_URL}/reminders`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...form, isDone: false }),
+      });
 
+      if (!response.ok) {
+        throw new Error(`Server responded with ${response.status}`);
+      }
+      const data = await response.json();
+      // setSelectedPetId(data)
+      console.log(data);
+      Navigate(`/`);
+    } catch (err) {
+      console.error("Failed to create reminder:", err);
+
+      setFormError("Failed to add the reminder.");
+    }
+  }
+
+  // function where which later is passed on the onChange inside input field
+  function onFormChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+    console.log(e.target.name, e.target.value);
+  }
   return (
-  <>
-  <Navbar />
-  <label htmlFor="pet-select">Choose a pet:</label>
-    <select name="pets" id="pet-select">
-        {pets.map((pet)=> 
-            <option key={pet.petId} value={pet.petName}> {pet.petName}</option>
-        )}
-    </select>
-  </>
-  )
-}
+    <>
+      <section className="content-panel">
+        <h2 className="page-header" style={{ justifyContent: "center" }}>
+          Reminder For Your Pet 🐶
+        </h2>
 
+        <form className="reminder-form" onSubmit={handleSubmit}>
+          {formError && <p className="error">{formError}</p>}
+
+          <label htmlFor="pet-select">Choose a pet</label>
+          <select
+            className="input-item"
+            name="petId"
+            id="pet-select"
+            value={form.petId}
+            onChange={onFormChange}
+          >
+            {pets.map((pet) => (
+              <option name="petId" key={pet.petId} value={pet.petId}>
+                {" "}
+                {pet.petName}
+              </option>
+            ))}
+          </select>
+
+          <label
+            style={{
+              display: "block",
+              alignself: "flex-start",
+              textAlign: "left",
+            }}
+          >
+            Enter Task
+          </label>
+          <input
+            type="text"
+            name="task"
+            className="input-item"
+            placeholder="eg: take puppy for walk"
+            value={form.task}
+            onChange={onFormChange}
+          />
+
+          <label>Any Notes</label>
+          <input
+            type="text"
+            name="notes"
+            className="input-item"
+            placeholder="Add any notes here"
+            value={form.notes}
+            onChange={onFormChange}
+          />
+          {/* <label>Status</label>
+    <input 
+    type="text"
+    name="isDone"
+    className="input-item"
+    placeholder="Is it done?"
+    value={form.isDone}
+    onChange={onFormChange}
+    /> */}
+
+          {/*   built in method to create the calender drop down. type="date"> HTML attribute value - HTML | MDN */}
+          {/* or both time and data <input type="datetime-local"> HTML attribute value - HTML | MDN */}
+          <label>Date And Time</label>
+          <input
+            type="datetime-local"
+            name="dueDate"
+            className="input-item"
+            placeholder="due"
+            value={form.dueDate}
+            onChange={onFormChange}
+          />
+          <button  type="submit" className="button button-primary">
+            Add Reminder
+          </button>
+        </form>
+      </section>
+    </>
+  );
+}
